bd63b040a02821351dc3489621338f42cbb9e135
Roger Pettett <rmp@psyphi.net>
2024-02-24 14:39:44 +0000

    support for specifying rtl device id (0-based), backwards-compatible, default 0

--- submit_job.sh
+++ submit_job.sh
@@ -44,7 +44,9 @@ if [ "$sat" == "METEOR-M23" ]; then
     frequency=${METEORM23}
     sampling=${METEOR_sampling}
 fi
-
+if [ "$device" == "" ]; then
+	device=0
+fi
 # Logging:
 echo `date --date=@${start} +%Y%m%d-%H%M%S` $sat $elevation >> recordings.log
 
@@ -52,10 +54,10 @@ echo `date --date=@${start} +%Y%m%d-%H%M%S` $sat $elevation >> recordings.log
 if [ "$specie" == "NOAA" ]; then
     
     # Record: (-p 0.0, 55.0 ppm ????, added -E dc -A fast)       
-    echo "timeout $rectime rtl_fm  -f ${frequency}M -s ${sampling}k -g ${NOAA_gain} -p 0.0 -E wav -E dc -E deemp -F 9 - | sox -t raw -e signed -c 1 -b 16 -r ${sampling}k - ${output}/noaa/${filename}.wav"  > job.txt 
+    echo "timeout $rectime rtl_fm  -d ${device} -f ${frequency}M -s ${sampling}k -g ${NOAA_gain} -p 0.0 -E wav -E dc -E deemp -F 9 - | sox -t raw -e signed -c 1 -b 16 -r ${sampling}k - ${output}/noaa/${filename}.wav"  > job.txt 
 
-    #echo "timeout $rectime rtl_fm  -f ${frequency}M -s 60k  -g 45 -E wav - | sox -t raw -e signed -c 1 -b 16 -r 60k - ${filename}.wav" # > job.txt
-    #echo "timeout $rectime rtl_fm  -f ${frequency}M -s 60k  -g 45 -p 55 -E wav -E deemp -F 9 - | sox -t raw - ${filename}.wav rate 11025" # > job.txt
+    #echo "timeout $rectime rtl_fm  -d ${device} -f ${frequency}M -s 60k  -g 45 -E wav - | sox -t raw -e signed -c 1 -b 16 -r 60k - ${filename}.wav" # > job.txt
+    #echo "timeout $rectime rtl_fm  -d ${device} -f ${frequency}M -s 60k  -g 45 -p 55 -E wav -E deemp -F 9 - | sox -t raw - ${filename}.wav rate 11025" # > job.txt
 
     # Resample and Decode:
     echo "/bin/bash apt.sh ${output}/noaa/${filename} &>> jobs.log" >> job.txt
@@ -73,22 +75,11 @@ if [ "$specie" == "METEOR-M" ] || [ "$specie" == "METEOR-M2" ]; then
 
     # Record:
     # (from https://www.reddit.com/r/RTLSDR/comments/abn29d/automatic_meteor_m2_reception_on_linux_using_rtl)
-    echo "timeout $rectime rtl_fm -M raw -f ${frequency}M -s ${sampling}k -g ${METEOR_gain} -p 0.0 | sox -t raw -r ${sampling}k -c 2 -b 16 -e s - -t wav ${output}/meteor/${filename}.wav rate 96k" >> job.txt
+    echo "timeout $rectime rtl_fm -d ${device} -M raw -f ${frequency}M -s ${sampling}k -g ${METEOR_gain} -p 0.0 | sox -t raw -r ${sampling}k -c 2 -b 16 -e s - -t wav ${output}/meteor/${filename}.wav rate 96k" >> job.txt
     
     # Resample and Decode:
     echo "/bin/bash lrpt.sh ${output}/meteor/${filename} &>> jobs.log" >> job.txt
 
-    # Old stuff:
-    #echo "timeout $rectime rtl_sdr -f ${frequency}M -s 140k -g 28 -d 0 -p 1 -b 8 audio/${filename}.bin"  > job.txt    ## tried 
-    # rtl_fm -M raw -s 140000 -f 137.9M -E dc -g <gain> -p <ppm> 
-    # rtl_fm -f 137.9M -s 140k -M raw -g <gain> -p <ppm> <output .wav file    
-    ##    echo "timeout $rectime rtl_fm  -f ${frequency}M -g 50 -s 200k - > audio/${filename}.raw" > job.txt
-    ##    echo "sox -r 192000 -e signed -b 16 -c 2 audio/${filename}.raw audio/${filename}.wav" >> job.txt
-    ##    echo "sox audio/${filename}.wav audio/${filename}_res.wav rate 140000" >> job.txt
-    ###    echo "export DISPLAY=:0" > job.txt
-    #echo "timeout $rectime python /home/franchini/Satellite/METEOR/meteor-m2-lrpt-hack/meteor-m2-lrpt.py --destfile /home/franchini/Satellite/METEOR/${filename}" >> job.txt
-    ###    echo "timeout $rectime /usr/bin/python /home/franchini/Satellite/METEOR/MexiMeteor2/top_block.py" >> job.txt
-    
     at $at_start -f job.txt &> /dev/null
     rm job.txt
     
